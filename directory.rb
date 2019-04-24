1a23fe4ec932e20baadc1f8e66fbb45fa769044e
Sam <samkitchen94@gmail.com>
2019-04-24 12:01:41 +0000

    added a load option to selection menu

--- directory.rb
+++ directory.rb
@@ -9,6 +9,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit"
 end
 
@@ -26,6 +27,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -41,6 +44,15 @@ def save_students
     student_data = [student[:name], student[:cohort], student[:age]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort, age = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym, age: age}
   end
   file.close
 end
