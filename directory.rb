224f5b93db4c222bdf4aaf7c30816b533d4546da
Sam <samkitchen94@gmail.com>
2019-04-24 10:58:58 +0000

    Interactive menu refactored

--- directory.rb
+++ directory.rb
@@ -1,3 +1,35 @@
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer 
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "Sorry I don't know what you mean, please enter your selection by number"
+  end
+end
+        
 def input_students
   puts "Time to enter the name of the students"
   puts "To finish, just hit return twice"
@@ -48,6 +80,7 @@ def print_footer
   end
 end
 
+interactive_menu
 input_students
 print_header
 print_students
